229a1371afc30b04bc8d408b0a33d024fd282e83
Vinicius Guedes <viniciusguedes.1007@outlook.com>
2026-04-09 14:04:01 -0300

    Alterando build dos Projetos para variados

---
+++ site/script/projetos.js
@@ -0,0 +1,13 @@
+const projetos = [
+  {
+    titulo: "Site Fernando Pessotto",
+    imagem: "./lib/images/projects/fernandopessotto.gif",
+    alt: "GIF do projeto Fernando Pessotto.",
+    descricao: "Site Desenvolvido para o Psicologo Fernando Pessotto",
+    tecnologias: "HTML - CSS - JS - JQUERY - PHP - MySQL - AdminLTE",
+    links: {
+      projeto: "https://fernandopessotto.com.br",
+      demo: "https://fernandopessotto.viniciusguedes.cloud"
+    }
+  },
+];
