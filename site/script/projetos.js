dd9837e3ab5ec138007b345c4a6ba786e58a1a26
Vinicius Guedes <viniciusguedes.1007@outlook.com>
2026-04-12 10:40:34 -0300

    Maxiprod

--- site/script/projetos.js
+++ site/script/projetos.js
@@ -10,4 +10,15 @@ const projetos = [
       demo: "https://fernandopessotto.viniciusguedes.cloud"
     }
   },
+  {
+    titulo: "Maxiprod - Teste",
+    imagem: "./lib/images/projects/maxiprod.gif",
+    alt: "GIF do projeto Maxiprod.",
+    descricao: "Site Desenvolvido para o Teste Técnico da Maxiprod",
+    tecnologias: "HTML - CSS - TYPESCRIPT - REACT - .NET - SQL Server - Docker",
+    links: {
+      github: "https://github.com/Vinicius-Guedes/Maxiprod",
+      demo: "https://fernandopessotto.viniciusguedes.cloud"
+    }
+  },
 ];
