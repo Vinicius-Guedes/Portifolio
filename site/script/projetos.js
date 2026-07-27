9f1a7961f53a95d16a59ad136c7598babe415153
Vinicius Guedes <viniciusguedes.1007@outlook.com>
2026-07-27 19:58:26 -0300

    Projeto Mangiare

--- site/script/projetos.js
+++ site/script/projetos.js
@@ -15,7 +15,7 @@ const projetos = [
     imagem: "./lib/images/projects/maxiprod.gif",
     alt: "GIF do projeto Maxiprod.",
     descricao: "Site Desenvolvido para o Teste Técnico da Maxiprod",
-    tecnologias: "HTML - CSS - TYPESCRIPT - REACT - .NET - SQL Server - Docker",
+    tecnologias: "HTML - CSS - TYPESCRIPT - REACT - .NET - SQL Server",
     links: {
       github: "https://github.com/Vinicius-Guedes/Maxiprod",
       demo: "https://maxiprod.viniciusguedes.cloud"
@@ -26,7 +26,7 @@ const projetos = [
     imagem: "./lib/images/projects/pizzaria.gif",
     alt: "GIF do projeto Pizzaria.",
     descricao: "Site Desenvolvido para aula de Programação do Luiz Rosa",
-    tecnologias: "HTML - CSS - JQUERY - BOOTSTRAP - .NET - SQL Server - Docker",
+    tecnologias: "HTML - CSS - JQUERY - BOOTSTRAP - .NET - SQL Server",
     links: {
       github: "https://github.com/LuizRosa-Aulas/Pizzaria.UI",
       demo: "https://pizzaria.viniciusguedes.cloud"
@@ -37,7 +37,7 @@ const projetos = [
     imagem: "./lib/images/projects/isabelamarquespsi.gif",
     alt: "GIF do projeto Isabela Marques.",
     descricao: "Site Desenvolvido para a Psicóloga Isabela Marques",
-    tecnologias: "HTML - CSS - JS - Docker",
+    tecnologias: "HTML - CSS - JS",
     links: {
       projeto: "https://isabelamarquespsi.com.br"
     }
@@ -49,7 +49,18 @@ const projetos = [
     descricao: "Site Desenvolvido para Monitoramento do Servidor",
     tecnologias: "HTML - CSS - JS - NODE",
     links: {
-      projeto: "https://monitoramento.viniciusguedes.cloud"
+      demo: "https://monitoramento.viniciusguedes.cloud",
+      github: "https://github.com/Vinicius-Guedes/Monitoramento"
+    }
+  },
+  {
+    titulo: "Mangiare Rosticceria",
+    imagem: "./lib/images/projects/mangiarerosticceria.mp4",
+    alt: "VIDEO do projeto Mangiare Rosticceria.",
+    descricao: "Site Desenvolvido para Monitoramento do Servidor",
+    tecnologias: "HTML - CSS - JS",
+    links: {
+      projeto: "https://mangiarerosticceria.com.br"
     }
   },
 ];
